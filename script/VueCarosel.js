26e05661162cd33d42bfc43287d55a6d3ef3cc76
enrico <rombaldonienrico@gmail.com>
2020-12-09 19:00:39 +0100

    next and prev

--- script/VueCarosel.js
+++ script/VueCarosel.js
@@ -3,11 +3,42 @@
 var app= new Vue ({
   el:'#app',
   data:{
+    imageIndex:0,
     images: [
       {
         imageUrl:'img/Aero-Technium_MgS-XTC_1250x1250px_V1.jpg',
         altText:"Aero-Technium",
       },
-    ]
+      {
+        imageUrl:'img/Fliegen_XD_1250x1250px_V1.jpg',
+        altText:"Fligen",
+      },
+      {
+        imageUrl:'img/StellaFJ_1250x1250px_V1.jpg',
+        altText:"Stella",
+      },
+      {
+        imageUrl:'img/Surfleader_CI4_1250x1250px_V1.png',
+        altText:"SurfLeader",
+      },
+    ],
+  },
+    methods: {
+      prevImg: function() {
+        this.imageIndex--;
+        if (this.imageIndex==-1) {
+          this.imageIndex= this.images.length-1;
+
+        }
+      },
+      nextImg: function() {
+        this.imageIndex++;
+          console.log(this.imageIndex);
+        if (this.imageIndex==this.images.length-1) {
+          this.imageIndex= 0;
+
+
+        }
+      },
   },
 });
